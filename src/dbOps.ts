3161bb0769d977e72540e5f1587467b07c78b69e
tim <tim@localhost.localdomain>
2024-01-28 11:08:18 +0700

    Test users are stored properly.

--- src/dbOps.ts
+++ src/dbOps.ts
@@ -10,66 +10,62 @@ dotenv.config({ path: envPath });
 
 // const connectionString = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
 
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
+const dbHost = process.env.DB_HOST as string;
+const dbPort = process.env.DB_PORT as string;
 
 const initMongoUrl = `mongodb://${dbHost}:${dbPort}/`;
 const poolSize = `?minPoolSize=1&maxPoolSize=10`;
 
-const dbNameAdmin = process.env.DB_NAME_ADMIN;
-const dbNameTricky = process.env.DB_NAME_TRICKY;
+const dbNameAdmin = process.env.DB_NAME_ADMIN as string;
+const dbNameTricky = process.env.DB_NAME_TRICKY as string;
 
 // This is to be transferred later to another module
 var url = `mongodb://${dbHost}:${dbPort}/`;
 logger.debug(`Mongo url is set to: ${url}`);
 
-async function findTestUser (email: string): Promise<boolean> {
+async function findTUser(e: string): Promise<boolean> {
     let result = false;
-    mongoose.connect(url).then
-    mongoose.connection.useDb(dbNameTricky! as string);
-    var query = TestUser.findOne({ email: email});
-    query.exec()
-    .then (user => {
-        logger.info(`The Query result is: ${JSON.stringify(user)}`);
-        if (user) {
-            result = true;
-        }
-    })
-    mongoose.disconnect;
+    let user = undefined;
+    await connectDB(dbNameTricky);
+    var query = TestUser.findOne({ email: e });
+    user = await query.exec()
+    if (user !== undefined) {
+        logger.info(`User found: ${JSON.stringify(user)}`);
+        result = true;
+    } else {
+        logger.info(`User with email: ${e} wasn't found.`)
+    }
+    disconnectDB;
     return result;
 }
 
-findTestUser("tim.timoff@gmail.com");
-
-// try {
-//   mongoose.connect(url);
-//   mongoose.connection.useDb(dbNameTricky!);
-//   if (mongoose.ConnectionStates.connected) {
-//     logger.debug(`Mongoose is connected...`);
-//   } else {
-//     logger.debug(`Mongoose is not connected...`);
-//   }
-// } catch (err) {
-//   logger.error(`Error connecting to database: ${(err as Error).message}.`);
-// } finally {
-//   mongoose.disconnect;
-// }
-
-// try {
-//     mongoose.connect(url);
-//     const conn = mongoose.connection.useDb("tricky");
-//     const meData = new TestUser({ email: 'tim.timoff@gmail.com' });
-//     // const me = createTUser(meData);
-//     // logger.info(`User created: ${JSON.stringify(me)} of type: ${typeof(me)}`);
-//     logger.info(`Retreiving info from db: ${conn.db.databaseName}, content: ${conn.db.collections.name.length}`);
-//   } catch (err) {
-//     logger.error(`Error creating test user: ${(err as Error).message}`);
-//   } finally {
-//     logger.info(`DB operations completed.`)
-//     mongoose.disconnect;
-// }
-
-async function createTUser(t: any) {
-  TestUser.create(t);
+async function createTUser(e: string) {
+    const data = { "email": e };
+    const n = new TestUser(data);
+    await connectDB(dbNameTricky);
+    const res = await findTUser(e);
+    logger.debug(`Find test user result: ${res}`);
+    if (res == false) {
+        try {
+            await n.save();
+            logger.info(`Test user: ${JSON.stringify(n)} created with ${JSON.stringify(data)}.`);
+        } catch (err) {
+            logger.error(`Error creating test user: ${(err as Error).message}`);
+        }
+    } else {
+        await disconnectDB();
+    }
 }
 
+async function connectDB(dbName: string) {
+    await mongoose.connect(url);
+    mongoose.connection.useDb(dbName);
+    logger.debug(`Connected to database ${dbName}`);
+}
+
+async function disconnectDB() {
+    mongoose.disconnect;
+    logger.debug(`Disconnected from database...`);
+}
+
+createTUser("y.timoshenkoff@yandex.ru");
